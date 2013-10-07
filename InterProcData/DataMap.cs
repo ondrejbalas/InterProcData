ecbac27041716f53ba2b359cb563bdff45ea7d40
Ondrej Balas <ondrej@ondrejbalas.com>
2013-10-07 17:08:34 -0400

    Initial (working) commit

---
+++ InterProcData/DataMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace InterProcData
+{
+    public class DataMap<T> : IDisposable
+    {
+        protected T SharedData;
+        protected MemoryMappedFile MemoryMap;
+
+        public DataMap(T sharedData)
+        {
+            SharedData = sharedData;
+        }
+
+        public void StartHostAsync(string name)
+        {
+            Task.Factory.StartNew(() => StartHost(name));
+        }
+
+        public void StartHost(string name)
+        {
+            var ms = new MemoryStream();
+            Serializer.SerializeWithLengthPrefix(ms, SharedData, PrefixStyle.Fixed32);
+            var buffer = ms.GetBuffer();
+
+            MemoryMap = MemoryMappedFile.CreateNew(name, ms.Length);
+
+            var stream = MemoryMap.CreateViewStream();
+            stream.Write(buffer, 0, (int)ms.Length);
+        }
+
+        public static T GetHostedData(string name)
+        {
+            var memoryMap = MemoryMappedFile.OpenExisting(name);
+
+            var stream = memoryMap.CreateViewStream();
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return Serializer.DeserializeWithLengthPrefix<T>(stream, PrefixStyle.Fixed32);
+        }
+
+        public void Dispose()
+        {
+            MemoryMap.Dispose();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 InterProcData
 =============
 
-This library is to help with inter-process communication on the same machine.
+This library is to help with inter-process communication within a single machine.
+
+Usage of this library assumes that you have two or more processes - a host process and one or more consumers.
+
+Usage Example - Host
+-------------------------
+
+```C#
+
+List<string> sharedData = Enumerable.Range(1, 100000).Select(x => string.Format("Item #{0}", x)).ToList();
+
+using (var host = new DataMap<List<string>>(sharedData))
+{
+    host.StartHost("someName");
+    Console.WriteLine("Hosting.. press enter to exit");
+    Console.ReadLine();
+}
+
+```
+
+Usage Example - Consumer
+-------------------------
+
+```C#
+
+var obj = DataMap<List<string>>.GetHostedData("someName");
+
+Console.WriteLine("Loaded {0} objects" + obj.Count);
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
+
+```
